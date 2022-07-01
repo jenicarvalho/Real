22e8f78c050186d8e93fa64ce8aa76d51d0853c4
Jeniffer Carvalho <jeniffer.carvalho@dowjones.com>
2022-06-30 22:03:41 -0300

    created useOutsideClick hook

---
+++ src/Hooks/useOutsideClick.ts
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from 'react'
+
+export const useOutsideClick = (callback: any) => {
+  const ref = useRef()
+
+  useEffect(() => {
+    const handleClick = (event: any) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback()
+      }
+    }
+    document.addEventListener('click', handleClick, true)
+
+    return () =>  document.removeEventListener('click', handleClick, true)
+  }, [ref])
+
+  return ref
+}
